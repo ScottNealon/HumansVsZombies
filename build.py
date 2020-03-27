6a88b408f484583f445e1369536f74edbacb92e6
Adithya Nott <adithya.nott@gmail.com>
2020-03-26 20:33:13 -0400

    Able to put together most of the nlogo file. Bug with writing info.md file in when using Python3

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import glob
+import io
 
 def valid_dev_dir(dev_dir):
     if not os.path.isdir(dev_dir) or not os.access(dev_dir, os.R_OK):
@@ -47,32 +49,82 @@ args = parser.parse_args()
 print(vars(args))
 
 class Assembler:
+    def __init__(self, dev_directory, print_debug=False):
+        self.dev_dir = dev_directory
+        self.print_debug = False
 
-    def generate_file():
-        with open('example_file.nlogo', 'w') as file:
-            # Code Tab X
-            print_delimiter_to_file(file)
-            # Interface Tab
-            print_delimiter_to_file(file)
-            # Info Tab X
-            print_delimiter_to_file(file)
-            # turtle shapes X
-            print_delimiter_to_file(file)
-            # NetLogo version X
-            print_delimiter_to_file(file)
-            # preview commands
-            print_delimiter_to_file(file)
-            # System Dynamics Modeler
-            print_delimiter_to_file(file)
-            # BehaviorSpace experiments X
-            print_delimiter_to_file(file)
-            # HubNet Client (unused atm)
-            print_delimiter_to_file(file)
-            # link shapes X
-            print_delimiter_to_file(file)
-            # model settings
-            print_delimiter_to_file(file)
-            # DeltaTick (left empty) XX
+    def print_delimiter(self, o_file):
+        if self.print_debug:
+            print("@#$#@#$#@")
+        if o_file is not None:
+            o_file.write("@#$#@#$#@\n")
 
-    def print_delimiter_to_file(file):
-        file.write("\n@#$#@#$#@")
+
+    def print_contents_directly_from_file(self, o_file, input_file_name):
+        with open(os.path.join(self.dev_dir, input_file_name), "r") as i_file:
+            for line in i_file:
+                if self.print_debug:
+                    print(line)  
+                if o_file is not None:
+                    o_file.write(line)
+
+    def print_single_behaviorspace_experiment(self, o_file, input_file_name):
+        with open(input_file_name, "r") as i_file:
+            for line in i_file:
+                if self.print_debug:
+                    print("  " + line)  
+                if o_file is not None:
+                    o_file.write("  " + line)
+        o_file.write("\n")
+
+    def print_behaviorspace_content(self, o_file):
+        if o_file is not None:
+            o_file.write("<experiments>\n")
+
+            behaviorspace_files = glob.glob(os.path.join(self.dev_dir, "behavior_space/*.xml"))
+            for file in behaviorspace_files:
+                self.print_single_behaviorspace_experiment(o_file, file)
+
+        if o_file is not None:
+            o_file.write("</experiments>\n")
+
+    def generate_file(self, output_file_name = None):
+        o_file = open(output_file_name, 'w') if output_file_name is not None else None
+        
+        # Code Tab X
+        self.print_delimiter(o_file)
+        # Interface Tab
+        self.print_contents_directly_from_file(o_file, 'interface/interface.txt')
+        self.print_delimiter(o_file)
+        # Info Tab X
+        self.print_contents_directly_from_file(o_file, 'info.md')
+        self.print_delimiter(o_file)
+        # turtle shapes X
+        self.print_delimiter(o_file)
+        # NetLogo version X
+        self.print_contents_directly_from_file(o_file, 'netlogo_version.txt')
+        self.print_delimiter(o_file)
+        # preview commands Section (unused atm)
+        self.print_contents_directly_from_file(o_file, 'preview_commands/preview_commands.txt')
+        self.print_delimiter(o_file)
+        # System Dynamics Modeler Section (unused atm)
+        self.print_contents_directly_from_file(o_file, 'system_dynamics_modeler/system_dynamics_modeler.txt')
+        self.print_delimiter(o_file)
+        # BehaviorSpace experiments X
+        self.print_behaviorspace_content(o_file)
+        self.print_delimiter(o_file)
+        # HubNet Client Section (unused atm)
+        self.print_contents_directly_from_file(o_file, 'hubnet_client/hubnet_client.txt')
+        self.print_delimiter(o_file)
+        # link shapes Section X
+        self.print_delimiter(o_file)
+        # model settings Section
+        self.print_contents_directly_from_file(o_file, 'model_settings.txt')
+        self.print_delimiter(o_file)
+        # DeltaTick Section (left empty)
+
+        if o_file is not None:
+            o_file.close()
+
+assembler = Assembler(args.dev_folder)
+assembler.generate_file("test.nlogo")
