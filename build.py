fb58b378425114a531c972a09f270224c242826c
Adithya Nott <adithya.nott@gmail.com>
2020-04-01 03:00:04 -0400

    most of work on disassembly done

--- build.py
+++ build.py
@@ -7,6 +7,22 @@ import shutil
 is_python3 = sys.version_info >= (3, 0)
 
 
+def valid_netlogo_file(nlogo_file):
+    if not os.path.exists(nlogo_file) or not os.access(nlogo_file, os.R_OK):
+        raise argparse.ArgumentTypeError("Readable NetLogo file to split up into dev directory is not adequately provided")
+
+    delimiter_count = 0
+    with open(nlogo_file, "r") as file:
+        while True:
+            line = file.readline()
+            if line.strip() == "@#$#@#$#@":
+                delimiter_count += 1
+            if not line:
+                break
+    if delimiter_count != 11:
+        raise argparse.ArgumentTypeError("Provided NetLogo file is not appropriately structured")
+    return nlogo_file    
+
 def valid_dev_dir(dev_dir):
     if not os.path.isdir(dev_dir) or not os.access(dev_dir, os.R_OK):
         raise argparse.ArgumentTypeError("Directory provided is not readable")
@@ -55,7 +71,7 @@ print(vars(args))
 class Assembler:
     def __init__(self, dev_directory, print_debug=False):
         self.dev_dir = dev_directory
-        self.print_debug = False
+        self.print_debug = print_debug
 
     def print_delimiter(self, o_file):
         if self.print_debug:
@@ -182,5 +198,116 @@ class Assembler:
         if o_file is not None:
             o_file.close()
 
-assembler = Assembler(args.dev_folder)
-assembler.generate_file("test.nlogo")
+class Disassembler:
+    def __init__(self, nlogo_file, dev_dir):
+        self.nlogo_file = nlogo_file
+        self.dev_dir = dev_dir
+
+    def write_directly_to_file_until_delimiter(self, nlogo_file, output_file_name):
+        with open(os.path.join(self.dev_dir, output_file_name), "w") as o_file:
+            # Need to actually, ya know, WRITE the files lol
+            lines = []
+            while True:
+                line = nlogo_file.readline()
+                if not line or line.strip() == "@#$#@#$#@":
+                    break
+                else:
+                    lines.append(line)
+
+            if len(lines) > 0:
+                lines[-1] = lines[-1].strip()
+            
+            o_file.writelines(lines)
+                
+
+    def write_shape_files(self, nlogo_file, link_shapes=True):
+        shape_type = "link_shapes" if link_shapes else "object_shapes"
+        while True:
+            lines = []
+            line = nlogo_file.readline()
+            if not line or line.strip() == "@#$#@#$#@":
+                break
+            else:
+                filename = line.strip() + ".txt"
+                while line and line.strip() != "" and line.strip() != "@#$#@#$#@":
+                    # reminder that strip below is just for debug printing. Strip above is intended though
+                    lines.append(line)
+                    line = nlogo_file.readline()
+
+                with open(os.path.join(self.dev_dir, shape_type, filename), "w") as o_file:
+                    if len(lines) > 0:
+                        lines[-1] = lines[-1].strip()
+                    o_file.writelines(lines)
+
+                if not line or line.strip() == "@#$#@#$#@":
+                    break
+
+        
+
+    def write_behaviorspace_files(self, nlogo_file):
+        import xml.etree.ElementTree as ET
+        import textwrap
+
+        full_xml_string = ""
+        while True:
+            line = nlogo_file.readline()
+            if not line or line.strip() == "@#$#@#$#@":
+                break
+            else:
+                # pass
+                full_xml_string += line
+        xml_tree = ET.fromstring(full_xml_string)
+        for child in xml_tree:
+            file_name = os.path.join(self.dev_dir, "behavior_space", child.attrib['name'].replace(" ", "_").lower() + ".xml")
+            print(file_name)
+            with open(file_name, "w") as experiment_file:
+                # TODO problem here fam: extra new line character at the end AND not indented properly
+                experiment_file.write(ET.tostring(child).decode("utf-8"))
+
+    def generate_dev_directory(self):
+        if os.path.exists(self.dev_dir):
+            shutil.rmtree(self.dev_dir);
+        else:
+            os.mkdir(self.dev_dir)
+        os.makedirs(os.path.join(self.dev_dir, "behavior_space"))
+        os.makedirs(os.path.join(self.dev_dir, "code"))
+        os.makedirs(os.path.join(self.dev_dir, "hubnet_client"))
+        os.makedirs(os.path.join(self.dev_dir, "interface"))
+        os.makedirs(os.path.join(self.dev_dir, "link_shapes"))
+        os.makedirs(os.path.join(self.dev_dir, "object_shapes"))
+        os.makedirs(os.path.join(self.dev_dir, "preview_commands"))
+        os.makedirs(os.path.join(self.dev_dir, "system_dynamics_modeler"))
+        
+
+        with open(self.nlogo_file, "r") as file:
+
+            self.write_directly_to_file_until_delimiter(file, "code/breeds.nls")
+            self.write_directly_to_file_until_delimiter(file, "interface/interface.txt")
+            self.write_directly_to_file_until_delimiter(file, "info.md")
+
+
+            #NEED TO IMPLEMENT
+            self.write_shape_files(file, False)
+
+            self.write_directly_to_file_until_delimiter(file, "netlogo_version.txt")
+
+            self.write_directly_to_file_until_delimiter(file, "preview_commands/preview_commands.txt")
+
+            self.write_directly_to_file_until_delimiter(file, "system_dynamics_modeler/system_dynamics_modeler.txt")
+
+            self.write_behaviorspace_files(file)
+
+            self.write_directly_to_file_until_delimiter(file, "hubnet_client/hubnet_client.txt")
+
+            self.write_shape_files(file, True)
+
+            self.write_directly_to_file_until_delimiter(file, "model_settings.txt")
+
+
+
+
+# assembler = Assembler(args.dev_folder)
+# assembler.generate_file("test.nlogo")
+
+disassembler = Disassembler("test.nlogo", "DSF")
+disassembler.generate_dev_directory()
